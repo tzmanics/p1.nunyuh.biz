13c5c506cb69773a24d7d2085652a4efa526b396
tzmanics <tzmanics@gmail.com>
2013-09-11 21:26:11 -0400

    Add style.css, Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-Welcome to P1
+<!doctype html>  
+<html> 
+<head>   
+	<title>T.Z.Manicsic : P1</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+	<link href='http://fonts.googleapis.com/css?family=Roboto:400,900,300' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<div id="name">
+		<h1>Tara Z. Manicsic</h1>
+	</div>
+
+	<div id ="bio">
+
+		<p>I'm a strong, inquisitive, scottish-filipino-serbian lady heading towards the end of my 20's.
+		I work as a part-time staff assistant to a bunch of grad. students, scientists, engineers, physicists, and 1 professor at an atmospheric research group.
+		I also work full-time at bettering myself as a crafter, cook, designer, developer and CS student at the Extension School.
+		I love long walks...up mountains, coding, creating, gourmand-ing and my dog, Toshi.</p>
+	</div>
+
+	<div id ="editor">
+		<h3>code editor : Sublime Text</h3>
+	</div>
+</body>
+</html>
